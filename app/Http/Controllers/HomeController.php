cb34730a2d8f9073dd1ef2d70bffe57ffe58e5ff
baqar naqvi <baqarnaqvi925@gmail.com>
2022-09-05 14:33:25 +0500

    image api configuration

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,7 +20,24 @@ class HomeController extends Controller
      * @return \Illuminate\View\View
      */
     public function index()
-    {
+    { $auth_user_email=auth()->user()->email;
+
+        //dd($auth_user_email);
+        $auth_user_email=auth()->user()->email;
+        $name=auth()->user()->first_name;
+
+         //dd($name);
+
+        $response = Http::post('https://209.97.168.200/hr/public/api/already_existing_user_role_api', [
+
+            'email' => $auth_user_email,
+
+        ]);
+        $avatar_file=$response['avatar_file'];
+        //dd($avatar_file);
+        Session::put('avatar_file', $avatar_file);
+        Session::put('name', $name);
+        //dd(Session::get('avatar_file'));
         return view('dashboard');
     }
 }
